302eb43573d4486c991617cc504439e6101e91a7
Jose <josevn1918@gmail.com>
2026-06-03 12:53:23 -0600

    fix: use express default export for vercel

--- api/index.js
+++ api/index.js
@@ -2,6 +2,4 @@ import { createApp } from "../server/src/app.js";
 
 const app = createApp();
 
-export default function handler(request, response) {
-  return app(request, response);
-}
+export default app;
